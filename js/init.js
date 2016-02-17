d46a532251921f75e6b123ed30f7df4348073cf3
luis rosario <lrosario101@gmail.com>
2016-02-16 23:47:28 -0500

    added smoothstate back in

--- js/init.js
+++ js/init.js
@@ -1,33 +1,50 @@
 var $ = require('jquery');
 var foundation = require('foundation');
 var cheet = require('cheet');
+var smoothstate = require('smoothstate');
+
+var archer = {
+  init: function () {
+    /**
+     * smooth scroll to a section of the page, original source below:
+     * http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links
+    */
+    var $body = $('html,body');
+    $('a[data-smooth-scroll]').on('click.smooth_scroll', function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $body.animate({
+            scrollTop: target.offset().top
+          }, 1000);
+
+          return false;
+        }
+      }
+    });
+  }
+}
 
 // initialize the things
 $(document).ready(function () {
 
-  var $body = $('html,body');
-
-  // say hi
+  // foundation
   $(document).foundation();
 
-  /**
-   * smooth scroll to a section of the page, original source below:
-   * http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links
-  */
-  $('a[data-smooth-scroll]').on('click.smooth_scroll', function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $body.animate({
-          scrollTop: target.offset().top
-        }, 1000);
+  // smooth scrolls and othe page things
+  archer.init();
 
-        return false;
-      }
+  // smoothstate
+  $('#container').smoothState({
+    onAfter: function($container, $newContent) {
+      // reinitialize foundation + our stuff
+      $newContent.foundation();
+      archer.init();
     }
   });
 
   // konami
   cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () { alert('Voilà!'); });
+
 });
